fff0e1f2bdde03a0d9c52211a9556abd6d34df05
Jose Perez <jjose.quevedo@gmail.com>
2023-05-08 16:58:23 -0300

    Adding comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,34 @@
+// Import the material package from Flutter.
 import 'package:flutter/material.dart';
 
+// The main function starts the app.
 void main() {
+  // Run the app with MyApp as the root widget.
   runApp(MyApp());
 }
 
+// MyApp is a stateful widget.
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _MyAppState();
 }
 
+// _MyAppState is the state of MyApp.
 class _MyAppState extends State<MyApp> {
+  // Define the initial index of the bottom navigation bar.
   int _currentIndex = 0;
 
-  final List<Widget> _children = [
-    Home(),
-    Settings(),
-    Profile(),
-    Notifications(),
-  ];
+  // Define the list of widgets to be shown in the bottom navigation bar.
+  final List<Widget> _children = [    Home(),    Settings(),    Profile(),    Notifications(),  ];
 
+  // This function is called when an item in the bottom navigation bar is tapped.
   void onTabTapped(int index) {
     setState(() {
       _currentIndex = index;
     });
   }
 
+  // The build method creates the widget tree.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -40,10 +44,13 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
       home: Scaffold(
+        // Add an app bar with a title.
         appBar: AppBar(
           title: Text('Flutter Bottom Navigation Demo'),
         ),
+        // Set the body of the scaffold to be the current widget in _children.
         body: _children[_currentIndex],
+        // Add a bottom navigation bar with four items.
         bottomNavigationBar: BottomNavigationBar(
           onTap: onTabTapped,
           currentIndex: _currentIndex,
@@ -71,6 +78,7 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
+// Home is a stateless widget.
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -83,6 +91,7 @@ class Home extends StatelessWidget {
   }
 }
 
+// Settings is a stateless widget.
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -95,6 +104,7 @@ class Settings extends StatelessWidget {
   }
 }
 
+// Profile is a stateless widget.
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -107,6 +117,7 @@ class Profile extends StatelessWidget {
   }
 }
 
+// Notifications is a stateless widget.
 class Notifications extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
